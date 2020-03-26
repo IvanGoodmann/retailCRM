cd017f9ea0d9bb91a62bdb08b7948dc6ef369741
Ivan <xdistil@gmail.com>
2020-03-26 19:04:12 +0400

    Готовы 4 секции

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,244 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <header>
+            <div class="container">
+                <div class="header-content">
+                    <div class="burger-box">
+                        <div class="burger-box__item"></div>
+                        <div class="burger-box__item"></div>
+                        <div class="burger-box__item"></div>
+                    </div>
+                    <div class="logo-box">
+                        <img src="@/assets/img/logo.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section class="block_01">
+            <div class="card-box obj">
+                <img src="@/assets/img/card.svg" alt="">
+            </div>
+            <div class="container">
+                <div class="content">
+                    <div class="title-box">
+                        Твоя премиум
+                        <div class="d-flex align-end">
+                            карта <span>Лучшая карта для <b>любителей путешествий</b></span>
+                        </div>
+                    </div>
+                    <div class="card-info">
+                        <div class="card-info__item">
+                            <div class="card-info__item-numb">
+                                11%
+                            </div>
+                            <div class="card-info__item-title">
+                                Вернем <br> за покупки <br> милями
+                            </div>
+                        </div>
+                        <div class="card-info__item">
+                            <div class="card-info__item-numb">
+                                0%
+                            </div>
+                            <div class="card-info__item-title">
+                                Бесплатное <br> снятие наличных <br> по всему миру
+                            </div>
+                        </div>
+                        <div class="card-info__item">
+                            <div class="card-info__item-numb">
+                                7%
+                            </div>
+                            <div class="card-info__item-title">
+                                До 7% <br> на остаток <br>по счету
+                            </div>
+                        </div>
+                        <div class="card-info__item">
+                            <div class="card-info__item-numb">
+                                0р
+                            </div>
+                            <div class="card-info__item-title">
+                                Бесплатное <br> годовое <br> обслуживание
+                            </div>
+                        </div>
+                    </div>
+                    <div class="button-box">
+                        <button class="custom-button">
+                            Заказать сейчас
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="block_02">
+            <div class="container">
+                <div class="content">
+                    <div class="content-title">
+                        Путешествие мечты начинается с CARD PLUS
+                    </div>
+                    <div class="content-subtitle">
+                        Мы продумали каждую мелочь. Владельцы CARD PLUS получают особый <span>пакет привилегий, необходимый
+                        для любого путешествия</span>
+                    </div>
+                    <div class="pluses-box">
+                        <div class="pluses-box__item">
+                            <div class="pluses-box__item-icon">
+                                <img src="@/assets/img/icon-plus_01.png" alt="">
+                            </div>
+                            <div class="pluses-box__item-title">
+                                Билеты любых авиакомпаний
+                            </div>
+                            <div class="pluses-box__item-description">
+                                Предложения от 300+ авиакомпаний, 185+ lowcost перевозчиков, чартеры от 60 крупных
+                                российских туроператоров
+                            </div>
+                        </div>
+                        <div class="pluses-box__item">
+                            <div class="pluses-box__item-icon">
+                                <img src="@/assets/img/icon-plus_02.png" alt="">
+                            </div>
+                            <div class="pluses-box__item-title">
+                                Мили больше не сгорают
+                            </div>
+                            <div class="pluses-box__item-description">
+                                Мили, накопленные при оплате картой AlfaTravel, не сгорают и остаются на вашем счете,
+                                пока вы не решите их потратить
+                            </div>
+                        </div>
+                        <div class="pluses-box__item">
+                            <div class="pluses-box__item-icon">
+                                <img src="@/assets/img/icon-plus_03.png" alt="">
+                            </div>
+                            <div class="pluses-box__item-title">
+                                Продвинутая страховка – бесплатно
+                            </div>
+                            <div class="pluses-box__item-description">
+                                Расширенная страховка для всей семьи с учетом экстремальных видов спорта и алкоголя
+                            </div>
+                        </div>
+                        <div class="pluses-box__item">
+                            <div class="pluses-box__item-icon">
+                                <img src="@/assets/img/icon-plus_04.png" alt="">
+                            </div>
+                            <div class="pluses-box__item-title">
+                                Доступ к счетам в разных валютах
+                            </div>
+                            <div class="pluses-box__item-description">
+                                Текущие счета в рублях, долларах США, евро, английских фунтах стерлингов и швейцарских
+                                франках открываются в любой момент
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="block_03">
+            <div class="container">
+                <div class="content">
+                    <div class="content-title">
+                        Мы позаботились, чтобы вы получили
+                    </div>
+                    <div class="takes-box">
+                        <div class="takes-box__item">
+                            <div class="takes-box__item-image">
+                                <img src="@/assets/img/take-image_01.png" alt="">
+                            </div>
+                            <div class="takes-box__item-title">
+                                Бесплатный безлимитный интернет в роуминге
+                            </div>
+                            <div class="takes-box__item-icons">
+                                <img src="@/assets/img/take-icon_01.png" alt="">
+                                <img src="@/assets/img/take-icon_02.png" alt="">
+                                <img src="@/assets/img/take-icon_03.png" alt="">
+                                <img src="@/assets/img/take-icon_04.png" alt="">
+                            </div>
+                        </div>
+                        <div class="takes-box__item">
+                            <div class="takes-box__item-image">
+                                <img src="@/assets/img/take-image_02.png" alt="">
+                            </div>
+                            <div class="takes-box__item-title">
+                                Бесплатная упаковка багажа
+                            </div>
+                            <div class="takes-box__item-subtitle">
+                                Держатели карт Visa Signature могут упаковать багаж в крупных аэропортах России
+                            </div>
+                        </div>
+                        <div class="takes-box__item">
+                            <div class="takes-box__item-image">
+                                <img src="@/assets/img/take-image_03.png" alt="">
+                            </div>
+                            <div class="takes-box__item-title">
+                                Бесплатный доступ в приложении App In The Air
+                            </div>
+                            <div class="takes-box__item-subtitle">
+                                Бесплатная премиум подписка при покупке авиабилетов на travel.alfabank.ru
+                            </div>
+                        </div>
+                        <div class="takes-box__item">
+                            <div class="takes-box__item-image">
+                                <img src="@/assets/img/take-image_04.png" alt="">
+                            </div>
+                            <div class="takes-box__item-title">
+                                Такси бизнес-класса со скидкой 20%
+                            </div>
+                            <div class="takes-box__item-icons">
+                                <img src="@/assets/img/take-icon_05.png" alt="">
+                                <img src="@/assets/img/take-icon_06.png" alt="">
+                            </div>
+                        </div>
+                        <div class="takes-box__item">
+                            <div class="takes-box__item-image">
+                                <img src="@/assets/img/take-image_05.png" alt="">
+                            </div>
+                            <div class="takes-box__item-title">
+                                Скидка в ресторанах Novikov Group в аэропорту Шереметьево
+                            </div>
+                        </div>
+                        <div class="takes-box__item">
+                            <div class="takes-box__item-image">
+                                <img src="@/assets/img/take-image_06.png" alt="">
+                            </div>
+                            <div class="takes-box__item-title">
+                                Доступ в бизнес-залы Priority Pass по всему миру
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="block_04">
+            <div class="container">
+                <div class="content">
+                    <div class="content-title">
+                        Персональный помощник
+                    </div>
+                    <div class="content-freetitle">
+                        Бесплатно для владельцев <span>CARD PLUS</span>
+                    </div>
+                    <div class="support-box">
+                        <div class="support-box__item">
+                            <div class="support-box__item-image">
+                                <img src="@/assets/img/girl.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // import OrderForm from './components/OrderForm.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      // OrderForm
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import './assets/scss/style.scss';
+    @import './assets/scss/fonts.scss';
 </style>
---
+++ src/components/OrderForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="form-box">
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "OrderForm"
+  }
+</script>
+
+<style scoped>
+
+</style>
